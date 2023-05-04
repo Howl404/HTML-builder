00a2321f4a257dc14275023161c828bdcd4a2db8
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-05-04 17:32:31 +0300

    03 done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path');
+
+fs.promises.readdir(__dirname + "/secret-folder", {withFileTypes: true})
+
+.then(data => {
+  data.forEach(function (item) {
+    if(item.isDirectory()) {
+      return;
+    } else {
+      fs.stat(__dirname + `/secret-folder/${item.name}`, function (err, stat) {
+        const size = stat.size;
+        const name = item.name.slice(0, item.name.indexOf('.'))
+        let extension = path.extname(__dirname + `/secret-folder/${item.name}`)
+        extension = extension.replace(".", '')
+        console.log(`${name} - ${extension} - ${size}bytes`);
+      });
+    }
+  })
+})
