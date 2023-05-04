63cca263f48e8cd06a144ebe0b91cefa6144d928
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-05-04 13:34:25 +0300

    01 done

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+let readable = fs.createReadStream(path.join(__dirname, './text.txt'));
+
+readable.on('data', (data) => {
+  console.log(data.toString());
+})
