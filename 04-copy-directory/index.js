74b2bb0fac6eab313b2735749484dd5330b61698
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-05-04 20:13:57 +0300

    04 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
 
+fs.promises.readdir(__dirname + "/files", { withFileTypes: true })
+
+.then(function (files) {
+  fs.rm(`${__dirname}/files-copy`, { recursive:true, force:true }, (err) => {
+
+    if (err) throw err;
+
+    fs.mkdir(__dirname + "/files-copy", { recursive:true }, (err) => {
+
+      if (err) throw err;
+
+      files.forEach(function (file) {
+        fs.copyFile(__dirname + `/files/${file.name}`, __dirname + `/files-copy/${file.name}`, function (err) {
+
+          if (err) throw err;
+          
+        })
+      })
+    })
+  })  
+})
