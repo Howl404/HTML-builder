84dd7400803d9aee5d83e811ca6402ba7d270232
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-05-07 22:52:32 +0300

    06 done

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -57,8 +57,82 @@ fs.rm(`${__dirname}/project-dist`, { recursive: true, force: true }, (err) => {
           );
         })
         .then(() => {
-          console.log(1);
+          fs.promises
+            .readdir(__dirname + '/styles', { withFileTypes: true })
+            .then((data) => {
+              let filtered = [];
+              data.forEach(function (file) {
+                let fileName = file.name;
+                let index = fileName.lastIndexOf('.');
+                if (fileName.slice(index + 1, fileName.length) !== 'css') {
+                  return;
+                } else {
+                  filtered.push(fileName);
+                }
+              });
+              return filtered;
+            })
+            .then((filtered) => {
+              fs.appendFile(__dirname + '/project-dist/style.css', '', (err) => {
+                if (err) throw err;
+
+                filtered.forEach(function (file) {
+                  let readable = fs.createReadStream(
+                    path.join(__dirname, `styles/${file}`)
+                  );
+
+                  readable.on('data', (data) => {
+                    fs.appendFile(
+                      __dirname + '/project-dist/style.css',
+                      data,
+                      (err) => {
+                        if (err) throw err;
+                      }
+                    );
+                  });
+                });
+              });
+            })
+            .then(() => {
+              fs.promises
+                .readdir(__dirname + '/assets', { withFileTypes: true })
+          
+                .then(function (files) {
+                  fs.mkdir(__dirname + '/project-dist', { recursive: true }, (err) => {
+                    if (err) throw err;
+          
+                    files.forEach(function (file) {
+                      copyFiles(file.name);
+                    });
+                  });
+                }
+              
+                );
+            });
         });
     });
   });
 });
+
+const copyFiles = function (directory) {
+  fs.promises
+    .readdir(__dirname + `/assets/${directory}`, { withFileTypes: true })
+
+    .then(function (files) {
+      fs.mkdir(__dirname + `/project-dist/assets/${directory}`, { recursive: true }, (err) => {
+        if (err) throw err;
+
+        files.forEach(function (file) {
+          fs.copyFile(
+            __dirname + `/assets/${directory}/${file.name}`,
+            __dirname + `/project-dist/assets/${directory}/${file.name}`,
+            function (err) {
+              if (err) throw err;
+            }
+          );
+        });
+      });
+    }
+    
+    );
+};
