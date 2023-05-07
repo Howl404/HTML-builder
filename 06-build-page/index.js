4773ea1180a5de99dc4a7c14972735bc79b87e8d
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-05-07 21:59:21 +0300

    06 html

---
+++ 06-build-page/index.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('node:path');
+let template = '';
+
+fs.rm(`${__dirname}/project-dist`, { recursive: true, force: true }, (err) => {
+  if (err) throw err;
+  fs.mkdir(`${__dirname}/project-dist`, (err) => {
+    if (err) throw err;
+    let readable = fs.createReadStream(path.join(__dirname, './template.html'));
+
+    readable.on('data', (data) => {
+      template = data.toString();
+      fs.promises
+        .readdir(__dirname + '/components', { withFileTypes: true })
+        .then((files) => {
+          const promises = files.map((file) => {
+            let fileName = file.name;
+            let index = fileName.lastIndexOf('.');
+            if (fileName.slice(index + 1, fileName.length) !== 'html') {
+              return Promise.resolve();
+            }
+            let readable = fs.createReadStream(
+              path.join(__dirname, `./components/${fileName}`)
+            );
+            return new Promise((resolve, reject) => {
+              let content = '';
+              readable.on('data', (data) => {
+                content += data.toString();
+              });
+              readable.on('end', () => {
+                resolve({ name: fileName.slice(0, index), content });
+              });
+              readable.on('error', reject);
+            });
+          });
+          return Promise.all(promises).then((components) => ({ components }));
+        })
+        .then(({ components }) => {
+          let lines = template.split('\n');
+          components.forEach(({ name, content }) => {
+            const identifier = `{{${name}}}`;
+            lines = lines.map(line => {
+              if (line.includes(identifier)) {
+                const whitespace = line.match(/^\s*/)[0];
+                const newContent = content.split('\n').map((componentLine) =>
+                  whitespace + componentLine).join('\n');
+                return newContent;
+              } else {
+                return line;
+              }
+            });
+          });
+          template = lines.join('\n');
+          return fs.promises.appendFile(
+            path.join(__dirname, '/project-dist/index.html'),
+            template.trim()
+          );
+        })
+        .then(() => {
+          console.log(1);
+        });
+    });
+  });
+});
